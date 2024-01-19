5ac132d233468aecc649043190c487aa84f528b1
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-01-19 17:18:30 -0500

    image path

--- src/projectImages/projectImage.js
+++ src/projectImages/projectImage.js
@@ -1,9 +1,12 @@
 import WhatIsGpt3Image from './WhatIsGpt3.png';
 import PortfolioImage from './Portfolio.png';
+import IndeKedInImage from './IndeKedIn.png';
 
 const imageMap = {
   'WhatIsGpt3.png': WhatIsGpt3Image,
   'Portfolio.png': PortfolioImage,
+  'IndeKedIn.png': IndeKedInImage,
+  
 };
 
 export default imageMap;
